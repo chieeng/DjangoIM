05d7922783976cd8cf4a57fa1d7289db563e4cff
terz. <terenceed.limpio@cit.edu>
2026-05-22 23:09:29 +0800

    Database

--- hotel_system_new/hotel_system/settings.py
+++ hotel_system_new/hotel_system/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'hotel_system.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hotel_db',
+        'USER': 'root',
+        'PASSWORD': 'root123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
